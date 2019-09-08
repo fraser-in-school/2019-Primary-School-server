d4e26817b2d887d35c973fbaa865f52ba842e6c9
zhanghao <zhanghao@orangelab.cn>
2019-09-09 05:30:34 +0800

    binaryKmeans can be used

--- binaryKmeans.py
+++ binaryKmeans.py
@@ -3,6 +3,10 @@ from pcap.proc.pcap import PcapHead
 from pcap.proc.util import BytesBuffer
 from pcap.proc.packet import Packet
 from pcap.proc.util import BytesOrder
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+from sklearn.metrics import silhouette_score  # 引入评价函数
+from sklearn.decomposition import PCA
 import os
 import pandas as pd
 from sklearn.cluster import KMeans
@@ -10,7 +14,7 @@ from sklearn.externals import joblib
 
 
 projectPath = os.path.abspath('')
-dataPath = os.path.join(projectPath, 'C:\\Users\\zhanghao\\Downloads\\大文件\\CompletePCAPs')
+dataPath = '/media/zhanghao/B6D488D4D48897EF/Users/zhanghao/Downloads/大文件/CompletePCAPs'
 
 # 获取目录下的所有文件名
 def get_data_file(dataPath):
@@ -34,14 +38,15 @@ def get_train_set(dataFile, trainData):
             trainData.append(data[:100])
         # 读取所有 dataFile 文件里面的前100个packet
         i += 1
-        if i >= 10:
+        if i >= 500:
             break
 
 
-def train(n, train_data, model, ):
-    km = KMeans(n_clusters=200)
-    km.fit(pd.DataFrame(train_data))
-    joblib.dump(km, './module/10.pkl')
+def train(n, trainData, model=None, ):
+    km = KMeans(n_clusters=n, n_jobs=-1)
+    km.fit(pd.DataFrame(trainData))
+    joblib.dump(km, './module/500-0.pkl')
+    return km
     # print('labers')
     # print(km.labels_)
     # print('predict')
@@ -49,6 +54,14 @@ def train(n, train_data, model, ):
     # print('score')
     # print(km.inertia_)
 
+
+def dim_reduction(trainData):
+    # 主成分提取降维
+    pcaClf = PCA(n_components=3, whiten=True)
+    pcaClf.fit(trainData)
+    # 降维后的数据
+    data = pcaClf.transform(trainData)
+    return data
 
 def parse(file, buffSize=2048):
     """
@@ -117,14 +130,206 @@ def test():
     trainData = []
     filePath = ''
     for file in files:
+        print(file)
         ext = file.split('.')[1]
         if ext == 'pcap':
             filePath = os.path.join(dataPath, file)
             get_train_set(dataFile=filePath, trainData=trainData)
-        print(filePath)
-        print(len(trainData))
-
+    # k = []
+    # score = []
+    print('length:')
+    print(len(trainData))
+    # for i in range(5, 200, 5):
+    #     km = train(i, trainData)
+    #     k.append(i)
+    #     score.append(km.inertia_)
+    # plt.scatter(k, score)
+    # plt.plot(k, score)
+    # plt.xlabel("k")
+    # plt.ylabel("distance")
+    # plt.show()
+    km = train(50, trainData)
+    data = dim_reduction(trainData)
+    drawPlot(data, km=km)
     # getTrainSet()
     # train()
 
-test()
+def drawPlot(data, km):
+    j = 0
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    # km = joblib.load('./module/100.pkl')
+    label_pred = km.labels_  # 获取聚类标签
+    centroids = km.cluster_centers_  # 获取聚类中心
+    inertia = km.inertia_  # 获取聚类准则的总和
+    mark = ['aliceblue',
+        'antiquewhite',
+        'aqua',
+        'aquamarine',
+        'azure',
+        'beige',
+        'bisque',
+        'black',
+        'blanchedalmond',
+        'blue',
+        'blueviolet',
+        'brown',
+        'burlywood',
+        'cadetblue',
+        'chartreuse',
+        'chocolate',
+        'coral',
+        'cornflowerblue',
+        'cornsilk',
+        'crimson',
+        'cyan',
+        'darkblue',
+        'darkcyan',
+        'darkgoldenrod',
+        'darkgray',
+        'darkgreen',
+        'darkkhaki',
+        'darkmagenta',
+        'darkolivegreen',
+        'darkorange',
+        'darkorchid',
+        'darkred',
+        'darksalmon',
+        'darkseagreen',
+        'darkslateblue',
+        'darkslategray',
+        'darkturquoise',
+        'darkviolet',
+        'deeppink',
+        'deepskyblue',
+        'dimgray',
+        'dodgerblue',
+        'firebrick',
+        'floralwhite',
+        'forestgreen',
+        'fuchsia',
+        'gainsboro',
+        'ghostwhite',
+        'gold',
+        'goldenrod',
+        'gray',
+        'green',
+        'greenyellow',
+        'honeydew',
+        'hotpink',
+        'indianred',
+        'indigo',
+        'ivory',
+        'khaki',
+        'lavender',
+        'lavenderblush',
+        'lawngreen',
+        'lemonchiffon',
+        'lightblue',
+        'lightcoral',
+        'lightcyan',
+        'lightgoldenrodyellow',
+        'lightgreen',
+        'lightgray',
+        'lightpink',
+        'lightsalmon',
+        'lightseagreen',
+        'lightskyblue',
+        'lightslategray',
+        'lightsteelblue',
+        'lightyellow',
+        'lime',
+        'limegreen',
+        'linen',
+        'magenta',
+        'maroon',
+        'mediumaquamarine',
+        'mediumblue',
+        'mediumorchid',
+        'mediumpurple',
+        'mediumseagreen',
+        'mediumslateblue',
+        'mediumspringgreen',
+        'mediumturquoise',
+        'mediumvioletred',
+        'midnightblue',
+        'mintcream',
+        'mistyrose',
+        'moccasin',
+        'navajowhite',
+        'navy',
+        'oldlace',
+        'olive',
+        'olivedrab',
+        'orange',
+        'orangered',
+        'orchid',
+        'palegoldenrod',
+        'palegreen',
+        'paleturquoise',
+        'palevioletred',
+        'papayawhip',
+        'peachpuff',
+        'peru',
+        'pink',
+        'plum',
+        'powderblue',
+        'purple',
+        'red',
+        'rosybrown',
+        'royalblue',
+        'saddlebrown',
+        'salmon',
+        'sandybrown',
+        'seagreen',
+        'seashell',
+        'sienna',
+        'silver',
+        'skyblue',
+        'slateblue',
+        'slategray',
+        'snow',
+        'springgreen',
+        'steelblue',
+        'tan',
+        'teal',
+        'thistle',
+        'tomato',
+        'turquoise',
+        'violet',
+        'wheat',
+        'white',
+        'whitesmoke',
+        'yellow',
+        'yellowgreen']
+    for i in label_pred:
+        ax.scatter([data[j:j + 1, 0]], [data[j:j + 1, 1]], [data[j:j + 1, 1]], mark[i])
+        j += 1
+    plt.show()
+
+def load_module(module=None):
+    km = joblib.load('./module/500-0.pkl')
+    file = os.path.join(dataPath, 'aim_chat_3a.pcap')
+    _pcap = Pcap()
+    _gen = _pcap.parse(file)
+    i = 0
+    testData = []
+    for pkt in _gen:
+        data = bytearray(pkt.data)
+        length = len(data)
+        # 长度不足100用0补齐，超过100取前100
+        if length < 100:
+            data = data + bytes([0] * (100 - length))
+            testData.append(data)
+        else:
+            testData.append(data[:100])
+        # 读取所有 dataFile 文件里面的前100个packet
+        i += 1
+        if i >= 100:
+            break
+    return km.predict(pd.DataFrame(testData))
+# test()
+
+load_module()
+
+
--- server.py
+++ server.py
@@ -1,14 +1,23 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import jsonify
 from flask import redirect, url_for
 from flask_bootstrap import Bootstrap
 from werkzeug.utils import secure_filename
+from sklearn.cluster import KMeans
+from sklearn.externals import joblib
+from pcap.proc.pcap import Pcap
 import os
+import pandas as pd
 
 app = Flask(__name__)
 bootStarp = Bootstrap(app)
 
+
+@app.errorhandler(404)
+def miss(e):
+    return render_template('pages-error-404.html'), 404
 
 @app.route('/index')
 def index():
@@ -22,13 +31,43 @@ def model_select():
 
 @app.route('/fileupload/pcapfile', methods=['POST', 'GET'])
 def upload():
+    result = ['failed']
     if request.method == 'POST':
         f = request.files['file']
         basepath = os.path.dirname(__file__)
         upload_path = os.path.join(basepath, 'static/uploads', secure_filename(f.filename))
         f.save(upload_path)
-        return redirect(url_for('index'))
-    return render_template('index.html')
+        result = load_module(upload_path).tolist()
+        res = {}
+        res['data'] = result
+        res['status'] = 200
+        print(result)
+        print(type(result))
+        return jsonify(result)
+    return jsonify(result)
+
+
+def load_module(file, module=None):
+    km = joblib.load('./module/500-0.pkl')
+    _pcap = Pcap()
+    _gen = _pcap.parse(file)
+    i = 0
+    testData = []
+    for pkt in _gen:
+        data = bytearray(pkt.data)
+        length = len(data)
+        # 长度不足100用0补齐，超过100取前100
+        if length < 100:
+            data = data + bytes([0] * (100 - length))
+            testData.append(data)
+        else:
+            testData.append(data[:100])
+        # 读取所有 dataFile 文件里面的前100个packet
+        i += 1
+        if i >= 100:
+            break
+    return km.predict(pd.DataFrame(testData))
+
 
 if __name__ == '__main__':
     app.run()
